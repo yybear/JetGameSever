00297124c18e044c569cca7e0866727ff810db48
ganq <qgan@v5.cn>
2014-06-24 14:57:30 +0800

    换目录

--- src/main/java/com/handwin/util/Main.java
+++
@@ -1,9 +0,0 @@
-package com.handwin.util;
-
-public class Main {
-	public static void main(String[] args) {
-		Hungary test=new Hungary();
-		System.out.println("最大匹配数："+test.findMatch());
-		test.printMatchInfo();
-	}
-}
